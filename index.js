f13a1ee0b588304c13350bbddcbcdd73c1784d1a
Alex Litvinenko <alex@MacBook-Air-Alex.local>
2024-08-14 18:43:18 +0500

    Объекты Введение

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-let i = 4;
-while (i < 4) {
-  console.log(i);
-  i = i++
+const phoneBook = {
+  'alex': 89944244242,
+  'artem': 89991234242,
+  'andrey': 89410029424,
 }
+
+console.log('Выводы данных из ТК:');
+console.log(phoneBook.alex);
+console.log(phoneBook['andrey']);
+console.log('_______________');
+console.log('Операции с ТК');
+phoneBook['popov'] = 89421231231;
+phoneBook.pepel = 81239422332;
+console.log(phoneBook);
+phoneBook.alex = 89999991234;
+delete phoneBook.artem;
+console.log(phoneBook);
