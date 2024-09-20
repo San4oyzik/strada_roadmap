5630ea08681cb43d37d40d2a87778caad9f43512
San4oyzik <san4oyzik@gmail.com>
2024-09-21 00:32:57 +0500

    ENV process

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
-const url = 'mongodb://localhost:27017/todo'; // урл для сервиса с mongodb
+require('dotenv').config();
+
+const url = process.env.DB_CONNECTION_STRING;
 const {MongoClient} = require('mongodb'); // конструктор клиентов mongodb
 const client = new MongoClient(url); // создаем новый клиент для работы с базой
 
 async function connectDB() {
   try {
-    await   client.connect(); // подключаемся к базе
+    await client.connect(); // подключаемся к базе
     console.log('Conneection to DB');
   } catch (error) {
     console.error(error);
