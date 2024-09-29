31cba58070a8ad492d3e72ca0bfe785288e94d9a
San4oyzik <san4oyzik@gmail.com>
2024-09-29 12:09:49 +0500

    Связь

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const db = require('../strada_roadmap/db')
-// const service = require('../strada_roadmap/service');
 const { ObjectId } = require('mongodb');
-const Task = require('./taskModel.js')
-// const Task = require('./addTask.js');
+const Task = require('./taskModel.js');
+const User = require('./userModel.js');
 app.use(express.json())
 
 app.use((req, res, next) => {
@@ -22,7 +21,8 @@ app.get('/', (req, res) => {
 
 app.get('/tasks', async (req, res) => {
   try {
-    const tasks = await db.getDB().collection('tasks').find({}).toArray();
+    const tasks = await Task.find({});
+    // const tasks = await db.getDB().collection('tasks').find({}).toArray();
     res.status(200).json(tasks)
   } catch (e) {
     console.error(e);
@@ -30,15 +30,50 @@ app.get('/tasks', async (req, res) => {
   }
 })
 
-app.post('/tasks', async (req, res) => {
+app.get('/users', async (req, res) => {
+  try {
+    const tasks = await User.find({});
+    // const tasks = await db.getDB().collection('tasks').find({}).toArray();
+    res.status(200).json(tasks)
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Error retrieving tasks');
+  }
+})
+
+app.get('/tasks/:taskId', async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.taskId);
+    if (!task) {
+      return res.status(404).send('Task not found');
+    }
+    const user = await User.findById(task.userId);
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    const taskWithUser = {
+      ...task.toObject(),
+      user,
+    };
+    delete taskWithUser.userId;
+    res.status(200).json(taskWithUser);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Error retrieving tasks');
+  }
+})
+
+app.post('/tasks/:userId', async (req, res) => {
   try {
     const nameTask = req.body.name;
     const statusTask = req.body.status;
     const priorityTask = req.body.priority;
+    const userId = req.params.userId;
     const task = await Task.create({
       name: nameTask,
       status: statusTask,
       priority: priorityTask,
+      userId: new ObjectId(userId)
     })
     // const task = new Task(nameTask, priorityTask);
     // await db.getDB().collection('tasks').insertOne(task)
@@ -47,6 +82,21 @@ app.post('/tasks', async (req, res) => {
     console.error(e);
   }
 })
+
+app.post('/users', async (req, res) => {
+  try {
+    const userName = req.body.name;
+    const userEmail = req.body.email;
+    const user = await User.create({
+      name: userName,
+      email: userEmail,
+    })
+    res.status(201).json({message: `User added ${userName}`, user});
+  } catch (e) {
+    console.error(e);
+  }
+})
+
 
 app.delete('/tasks/:taskId', async (req, res) => {
   try {
--- taskModel.js
+++ taskModel.js
@@ -4,6 +4,7 @@ const taskSchema = new mongoose.Schema({
   name: String,
   status: String,
   priority: String,
+    userId: { type: 'ObjectId', ref: 'User' }
 });
 
 const Task = mongoose.model('Task', taskSchema);
