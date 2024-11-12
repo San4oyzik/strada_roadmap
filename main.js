9a6f11834e612422aa4af453f132c9fedef9bb9a
San4oyzik <san4oyzik@gmail.com>
2024-11-12 09:42:38 +0500

    ts

---
+++ main.js
@@ -0,0 +1,8 @@
+function add(a, b) {
+    return a + b - a;
+}
+add(5, 9);
+var isHello = function (name) {
+    console.log(name + 'привет');
+};
+isHello('Alex');
---
+++ main.ts
@@ -0,0 +1,12 @@
+function add(a: number, b: number) {
+  return a + b - a
+}
+
+add(5, 9);
+
+
+const isHello = (name: string) => {
+  console.log(name + 'привет');
+};
+
+isHello('Alex');
